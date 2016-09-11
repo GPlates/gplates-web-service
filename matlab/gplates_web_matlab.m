d78135531c1c998cad60ae834524a54de967ca81
siwill22 <siwill22@gmail.com>
2016-09-11 13:38:46 +1000

    first commit of matlab example

---
+++ matlab/gplates_web_matlab.m
@@ -0,0 +1,34 @@
+
+recon_time = 60;
+js = urlread(['http://127.0.0.1:8000/reconstructions/coastlines/?time=', ...
+    num2str(recon_time)]);
+cs = loadjson(js);
+js = urlread(['http://127.0.0.1:8000/reconstructions/static_polygons/?time=', ...
+    num2str(recon_time)]);
+sp = loadjson(js);
+js = urlread(['http://127.0.0.1:8000/reconstructions/plate_polygons/?time=', ...
+    num2str(recon_time)]);
+pp = loadjson(js);
+
+figure
+hold on
+box on
+
+for j=1:length(pp.features)
+    xy = pp.features{j}.geometry.coordinates;
+    fill(xy(:,1),xy(:,2),[0.6,0.6,0.9])
+end
+for j=1:length(sp.features)
+    xy = sp.features{j}.geometry.coordinates;
+    fill(xy(:,1),xy(:,2),[0.2,0.2,0.2],'facealpha',0.5)
+end
+for j=1:length(cs.features)
+    xy = cs.features{j}.geometry.coordinates;
+    fill(xy(:,1),xy(:,2),[0.5,0.8,0.5])
+end
+for j=1:length(pp.features)
+    xy = pp.features{j}.geometry.coordinates;
+    plot(xy(:,1),xy(:,2),'r','linewidth',1.5)
+end
+
+axis equal tight xy
