db2c79eee5fcff5007feacbabeed300920e3bbb0
michaelchin <mchin@ip-172-31-0-35.ap-southeast-2.compute.internal>
2016-10-24 04:07:54 +0000

    python path error

--- django/GWS/GWS/wsgi.py
+++ django/GWS/GWS/wsgi.py
@@ -13,13 +13,13 @@ from django.core.wsgi import get_wsgi_application
 
 paths = [
             '/usr/lib/pygplates/revision12/',
-            os.path.dirname(os.path.realpath(__file__))
+             os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         ]
 for path in paths:
     if path not in sys.path:
         sys.path.insert(0, path)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GWS.settings")
 application = get_wsgi_application()
 
 
