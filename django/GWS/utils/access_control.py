a659f12aecd087372dfa4047bbec450923aae6d9
Michael Chin <mchin@gplates.org>
2016-10-31 02:58:00 +0000

    send request to gplates portal to request access

--- django/GWS/utils/access_control.py
+++ django/GWS/utils/access_control.py
@@ -1,16 +1,40 @@
 from functools import wraps
 
+from django.http import HttpResponse, HttpResponseForbidden
 from django.utils.decorators import available_attrs
 from django.conf import settings
+
+import requests
 
 def request_access(function=None, url=settings.ACCESS_CONTROL_URL):
     """
         the decorator to check access privilege
     """
+    url = 'http://130.56.249.211:7777/access_control/request_access/'
     def request_access_decorator(func):
         @wraps(func, assigned=available_attrs(func))
         def function_wrapper(request, *args, **kwargs):
-            return func(request, *args, **kwargs)
+            if not url:
+                return func(request, *args, **kwargs)
+            else:
+                print request.path_info
+                apikey = None
+                if request.method == 'GET':
+                    apikey = request.GET.get('apikey', None)
+                elif request.method == 'POST': 
+                    apikey = request.POST.get('apikey', None)
+                
+                if not apikey:    
+                    return HttpResponseForbidden()
+    
+                payload = {'path': request.path_info, 'key': apikey}
+    
+                r = requests.get(url, params=payload)
+    
+                if r.status_code != 200:
+                    return HttpResponseForbidden()
+                else:
+                    return func(request, *args, **kwargs)
         return function_wrapper
 
     #in order to support both @request_access and @request_access()
