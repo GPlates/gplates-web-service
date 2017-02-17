66f80f8c3e7ddccdc56aa75098bc2ce55d9a2d2d
michaelchin <michael.chin@sydney.edu.au>
2017-02-17 03:50:56 +0000

    (GWS)Remove the trailing slashes

--- django/GWS/topology/urls.py
+++ django/GWS/topology/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^plate_boundaries/',views.get_topological_boundaries, name='plate_boundaries'),
-    url(r'^plate_polygons/',views.get_plate_polygons, name='plate_polygons')
+    url(r'^plate_boundaries',views.get_topological_boundaries, name='plate_boundaries'),
+    url(r'^plate_polygons',views.get_plate_polygons, name='plate_polygons')
 ]
 
