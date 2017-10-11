84fd84d42ec0895f1dd075e7b0d8798d4bd47d2d
siwill22 <siwill22@gmail.com>
2017-10-11 16:33:40 +1100

    add experimental function to return reconstructed vips

--- django/GWS/earth/urls.py
+++ django/GWS/earth/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^litho1/?$',views.litho1, name='litho1'),
     url(r'^paleolithology/?$',views.paleolithology, name='paleolithology'),
+    url(r'^paleomagnetism/?$',views.paleomagnetic_poles, name='paleomagnetism'),
 ]
 
--- django/GWS/earth/views.py
+++ django/GWS/earth/views.py
@@ -72,10 +72,20 @@ def paleolithology(request):
 
     static_polygons_filename = str('%s/%s/%s' % (settings.MODEL_STORE_DIR,model,model_dict['StaticPolygons']))
 
+    #select points based on age before doing plate id assignment - should be quicker??
+    paleolithology_points = pygplates.FeatureCollection(paleolithology_datafile)
+
+    valid_points = []
+    for point in paleolithology_points:
+        if point.get_valid_time()[0]>float(time) and point.get_valid_time()[1]<=float(time):
+            valid_points.append(point)
+
+    print valid_points
+
     assigned_features = pygplates.partition_into_plates(
         static_polygons_filename,
         rotation_model,
-        paleolithology_datafile,
+        valid_points,
         properties_to_copy = [pygplates.PartitionProperty.reconstruction_plate_id],
         partition_method = pygplates.PartitionMethod.most_overlapping_plate
     )
@@ -99,4 +109,58 @@ def paleolithology(request):
     #The "*" makes the service wide open to anyone. We should implement access control when time comes. 
     response['Access-Control-Allow-Origin'] = '*'
     return response
+
+
+@csrf_exempt
+def paleomagnetic_poles(request):
+
+    anchor_plate_id = request.GET.get('pid', 0)
+    time = request.GET.get('time', 0)
+    model = request.GET.get('model',settings.MODEL_DEFAULT)
+
+    model_dict = get_reconstruction_model_dict(model)
+
+    rotation_model = pygplates.RotationModel([str('%s/%s/%s' %
+        (settings.MODEL_STORE_DIR,model,rot_file)) for rot_file in model_dict['RotationFile']])
+
+    vgp_datafile = '%s/T2012_VGPs.gpmlz' % settings.PALEO_STORE_DIR
+
+    static_polygons_filename = str('%s/%s/%s' % (settings.MODEL_STORE_DIR,model,model_dict['StaticPolygons']))
+
+    #select points based on age before doing plate id assignment - should be quicker??
+    vgp_points = pygplates.FeatureCollection(vgp_datafile)
+
+    #valid_points = []
+    #for point in vgp_points:
+    #    if point.get_valid_time()[0]>time and point.get_valid_time()[1]<time:
+    #        valid_points.append(point)
+
+    assigned_features = pygplates.partition_into_plates(
+        static_polygons_filename,
+        rotation_model,
+        vgp_points,
+        properties_to_copy = [pygplates.PartitionProperty.reconstruction_plate_id],
+        partition_method = pygplates.PartitionMethod.most_overlapping_plate
+    )
+
+    reconstructed_vgps = []
+    pygplates.reconstruct(
+        assigned_features, 
+        rotation_model, 
+        reconstructed_vgps,
+        float(time),
+        anchor_plate_id=anchor_plate_id)
+
+    ret = write_json_reconstructed_point_features(reconstructed_vgps,
+                                                  attributes=None)
+
+    #add header for CORS
+    #http://www.html5rocks.com/en/tutorials/cors/
+    response = HttpResponse(ret, content_type='application/json')
+    
+    #TODO: 
+    #The "*" makes the service wide open to anyone. We should implement access control when time comes. 
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
+
 
