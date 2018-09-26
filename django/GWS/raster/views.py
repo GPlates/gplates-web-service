30fb08c358bea9adfc367f4f10a2c26d85f4bd34
michaelchin <michael.chin@sydney.edu.au>
2018-09-26 16:57:30 +1000

    implement raster query

--- django/GWS/raster/views.py
+++ django/GWS/raster/views.py
@@ -5,7 +5,31 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseServerError, HttpResponseBadRequest
+from django.db import connection
+
+import logging, traceback
+logger = logging.getLogger('dev')
 
 def query(request):
-    return HttpResponse("hello query!") 
+    try:
+        lon = float(request.GET['lon'])
+        lat = float(request.GET['lat'])
+        raster_name = request.GET['raster_name']
+    except:
+        logger.error(traceback.format_exc())
+        return HttpResponseBadRequest('Bad raster query!')
+
+    lon = (lon + 360)%360   
+ 
+    with connection.cursor() as cursor:
+        cursor.execute('''SELECT rid,ST_Value(rast, ST_SetSRID(ST_MakePoint({0},{1}),4326), false) AS val
+            FROM public.{2}
+            WHERE ST_Intersects(rast, ST_SetSRID(ST_MakePoint({0},{1}),4326))'''.format(lon, lat, raster_name)
+        )
+        row = cursor.fetchone()
+        if row:
+            return HttpResponse(row[1])
+        else:
+            return HttpResponseServerError('Failed to query raster.')
+    return HttpResponse("some incredible thing just happened!!!") 
