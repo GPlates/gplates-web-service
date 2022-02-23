02432c9178caaa98f1f808800188677efa20a071
michaelchin <michaelchin78@hotmail.com>
2022-02-23 18:44:10 +1100

    return nan when raster query did not return anything

--- django/GWS/raster/views.py
+++ django/GWS/raster/views.py
@@ -22,25 +22,26 @@ def query(request):
 
     lon = (lon + 360)%360   
  
-    with connection.cursor() as cursor:
-        cursor.execute('''SELECT rid,ST_Value(rast, ST_SetSRID(ST_MakePoint({0},{1}),4326), false) AS val
-            FROM public.{2}
-            WHERE ST_Intersects(rast, ST_SetSRID(ST_MakePoint({0},{1}),4326))'''.format(lon, lat, raster_name)
-        )
-        row = cursor.fetchone()
-        
-        if row:
-            ret = row[1]
-            if math.isnan(ret) or ret < 0:        
-                ret = 'nan'
-            else:
-                ret = '%.2f' % ret 
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('''SELECT rid,ST_Value(rast, ST_SetSRID(ST_MakePoint({0},{1}),4326), false) AS val
+                FROM public.{2}
+                WHERE ST_Intersects(rast, ST_SetSRID(ST_MakePoint({0},{1}),4326))'''.format(lon, lat, raster_name)
+            )
+            row = cursor.fetchone()
             
+            ret='nan'
+            if row:
+                ret = row[1]
+                if not math.isnan(ret) and ret >= 0:        
+                    ret = '%.2f' % ret 
+                
             response = HttpResponse(ret)
             #TODO: 
             #The "*" makes the service wide open to anyone. We should implement access control when time comes. 
             response['Access-Control-Allow-Origin'] = '*'
             return response
-        else:
-            return HttpResponseServerError('Failed to query raster.')
-    return HttpResponse("some incredible thing just happened!!!") 
+    except:
+        logger.error(traceback.format_exc())    
+        return HttpResponseServerError('Failed to query raster.')
+    
