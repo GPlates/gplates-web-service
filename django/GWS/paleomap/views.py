b0aa08fa2931eee51f4f7c525e5ba4996aeada0a
michael <michael.chin@sydney.edu.au>
2022-06-28 21:26:18 +1000

    update docker container to ubuntu 22:04

--- django/GWS/paleomap/views.py
+++ django/GWS/paleomap/views.py
@@ -12,7 +12,7 @@ import subprocess
 
 import matplotlib
 matplotlib.use('Agg')
-from mpl_toolkits.basemap import Basemap
+#from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from matplotlib.collections import LineCollection
