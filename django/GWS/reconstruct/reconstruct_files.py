81ac8a0ce7e6c80ff3b2d6cf61f523f47535b41f
michael <michael.chin@sydney.edu.au>
2022-07-17 21:16:34 +1000

    re-orgnize files

--- django/GWS/reconstruct/reconstruct_files.py
+++ django/GWS/reconstruct/reconstruct_files.py
@@ -1,16 +1,21 @@
+#
+# put your copyright, software license and legal information here.
+#
+import glob
 import io
 import os
 import sys
+import tempfile
 import traceback
 import zipfile
+from pathlib import Path
 
 import pygplates
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
 from django.views.decorators.csrf import csrf_exempt
 from utils.get_model import get_reconstruction_model_dict
-from pathlib import Path
-import tempfile, glob
+
 
 #
 # reconstruct uploaded files
@@ -30,20 +35,22 @@ def reconstruct(request):
 
         # create temporary dir to save intermediate files, remember to remove it after use
         with tempfile.TemporaryDirectory(prefix="gws-recon-files-tmp") as tmp_dir:
-            print(tmp_dir)
-
             # save the upload files into the temporary dir
             save_upload_files(request, tmp_dir)
 
             # get parameters from the http post request
-            time, model, assign_plate_id_flag = get_request_parameters(request)
+            time, model, assign_plate_id_flag, output_filename = get_request_parameters(
+                request
+            )
+
+            # find and unzip all zip files
+            find_and_unzip_all_zip_files(tmp_dir)
 
             reconstructable_files = []
             reconstructable_files = glob.glob(f"{tmp_dir}/**/*.shp", recursive=True)
-            print(reconstructable_files)
-            # (name, ext) = os.path.splitext(f.name)
-            # if ext in [".shp", ".gpml", ".gpmlz"]:
-            # reconstructable_files.append(f.name)
+            reconstructable_files += glob.glob(f"{tmp_dir}/**/*.gpml", recursive=True)
+            reconstructable_files += glob.glob(f"{tmp_dir}/**/*.gpmlz", recursive=True)
+            # print(reconstructable_files)
 
             model_dict = get_reconstruction_model_dict(model)
             if not model_dict:
@@ -66,8 +73,7 @@ def reconstruct(request):
             output_path = f"{tmp_dir}/output/"
             Path(output_path).mkdir(parents=True, exist_ok=True)
 
-            assign_plate_id_flag = True
-            if assign_plate_id_flag:
+            if assign_plate_id_flag:  # assign plate id and then reconstruct
                 feature_collection = pygplates.FeatureCollection()
                 for f in reconstructable_files:
                     # print(f)
@@ -87,37 +93,40 @@ def reconstruct(request):
                 pygplates.reconstruct(
                     feature_collection,
                     rotation_model,
-                    f"{output_path}/reconstructed-{time}Ma.shp",
+                    f"{output_path}/{time}Ma.shp",
                     float(time),
                 )
-            else:  # when assign_plate_id_flag = False
-                print(reconstructable_files)
-                print(f"{output_path}/reconstructed-{time}Ma.shp")
+            else:  # when assign_plate_id_flag = False, do not assign plate id, just reconstruct straightaway
                 pygplates.reconstruct(
                     reconstructable_files,
                     rotation_model,
-                    f"{output_path}/reconstructed-{time}Ma.shp",
+                    f"{output_path}/{time}Ma.shp",
                     float(time),
                 )
 
             # print(os.listdir(output_path))
             s = io.BytesIO()
             with zipfile.ZipFile(s, "w") as zf:
-                for r, d, files in os.walk(output_path):
+                for r, _, files in os.walk(output_path):
                     if not files:
-                        return HttpResponseServerError(
-                            "Error: No output files have been created!"
+                        error_msg = (
+                            f"Error: No output files have been created!"
+                            + "You might need to add 'assign_plate_id=1' to your http request."
                         )
+                        print(error_msg)
+                        return HttpResponseServerError(error_msg)
                     for f in files:
-                        print(os.path.join(r, f))
-                        zf.write(str(os.path.join(r, f)), f)
+                        # print(os.path.join(r, f))
+                        zf.write(
+                            str(os.path.join(r, f)), f
+                        )  # the second argument is the file path inside the zip archive.
 
             response = HttpResponse(
                 s.getvalue(), content_type="application/x-zip-compressed"
             )
             response[
                 "Content-Disposition"
-            ] = "attachment; filename=reconstructed_files.zip"
+            ] = f"attachment; filename={output_filename}.zip"
 
             return response
     except:
@@ -154,10 +163,19 @@ def save_upload_files(request, tmp_dir):
 def get_request_parameters(request):
     time = request.POST.get("time", None)
     model = request.POST.get("model", settings.MODEL_DEFAULT)
-    assign_plate_id_flag = request.POST.get("assign_plate_id", False)
+    out_fn = request.POST.get("filename", "reconstructed_files")
+    assign_plate_id_flag = request.POST.get("assign_plate_id", True)
     try:
-        if 1 == int(assign_plate_id_flag):
-            assign_plate_id_flag = True
+        if 0 == int(assign_plate_id_flag):
+            assign_plate_id_flag = False
     except:
-        pass  # do nothing, the default assign_plate_id_flag is False
-    return time, model, assign_plate_id_flag
+        pass  # do nothing, the default assign_plate_id_flag is True
+    return time, model, assign_plate_id_flag, out_fn
+
+
+# find and unzip all zip files
+def find_and_unzip_all_zip_files(tmp_dir):
+    zip_files = glob.glob(f"{tmp_dir}/**/*.zip", recursive=True)
+    for zip_file in zip_files:
+        with zipfile.ZipFile(zip_file, "r") as zip_ref:
+            zip_ref.extractall(tmp_dir)
--- examples/reconstruct_files_with_web_service.py
+++ examples/reconstruct_files_with_web_service.py
@@ -15,19 +15,23 @@ script_path = os.path.dirname(os.path.realpath(__file__))
 output_path = f"{script_path}/output"
 Path(output_path).mkdir(parents=True, exist_ok=True)
 
+data_folder = f"{script_path}/data"
 files = {
-    "file_1": open(f"{script_path}/Australia_Points.shx", "rb"),
-    "file_2": open(f"{script_path}/Australia_Points.prj", "rb"),
-    "file_3": open(f"{script_path}/Australia_Points.shp", "rb"),
-    "file_4": open(f"{script_path}/Australia_Points.dbf", "rb"),
+    "file_1": open(f"{data_folder}/Australia_Points.shx", "rb"),
+    "file_2": open(f"{data_folder}/Australia_Points.prj", "rb"),
+    "file_3": open(f"{data_folder}/Australia_Points.shp", "rb"),
+    "file_4": open(f"{data_folder}/Australia_Points.dbf", "rb"),
 }
+
+# You may compess the shapefiles into a zip file and use the line below
+# files = {"file_1": open(f"{data_folder}/Australia_Points.zip", "rb")}
 
 data = {"time": 100}
 
 r = requests.post(url, files=files, data=data)
 print(r.reason)
 
-with open(f"{script_path}/result.zip", "wb") as of:
+with open(f"{output_path}/result.zip", "wb") as of:
     of.write(r.content)
 
 print("done!")
