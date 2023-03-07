ef671c498e9c1e23e977d970e70d0adbd4984785
michael <michael.chin@sydney.edu.au>
2023-03-07 16:08:30 +1100

    use raster id to define vector layers

--- django/GWS/mobile/get_vector_layers.py
+++ django/GWS/mobile/get_vector_layers.py
@@ -8,9 +8,17 @@ data_path = f"{settings.BASE_DIR}/DATA/mobile-app/"
 
 
 def get_layers(request):
-    '''get vector layers for a given model'''
-    model_name = request.GET.get("model", "SETON2012")
-    with open(f"{data_path}/{model_name}-layers.json", "r") as f:
+    '''
+    get vector layers for a given raster ID.
+    if the vector layers do not exist, return the default ones.
+    '''
+    raster_id = request.GET.get("raster", None)
+
+    cfg_file = f"{data_path}/{raster_id}-vector-layers.json"
+    if not os.path.isfile(cfg_file):
+        cfg_file = f"{data_path}/default-vector-layers.json"
+
+    with open(cfg_file, "r") as f:
         response = HttpResponse(
             f, content_type="application/json; charset=utf8")
         response["Access-Control-Allow-Origin"] = "*"
