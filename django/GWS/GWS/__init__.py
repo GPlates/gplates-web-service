0acb5e076e46d87033d37e66c8931957e29304cb
michaelchin <michaelchin78@hotmail.com>
2024-01-24 10:34:06 +1100

    add .github/workflows/test.yml

--- django/GWS/GWS/__init__.py
+++ django/GWS/GWS/__init__.py
@@ -4,8 +4,9 @@ from pathlib import Path
 
 from django.conf import settings
 
-__version__ = "1.0.0"
+__version__ = "0.2.0"
 
+# download the plate models if the "model repo" directory does not exist.
 if not os.path.isdir(settings.MODEL_REPO_DIR):
     Path(settings.MODEL_REPO_DIR).mkdir(parents=True, exist_ok=True)
     subprocess.call(["pmm", "download", "all", settings.MODEL_REPO_DIR])
