168d1cf33acc3a8f61ff1e8be7ee493527aa1731
michaelchin <michaelchin78@hotmail.com>
2024-01-24 16:58:06 +1100

    test action 17

--- test/setup-db.sh
+++ test/setup-db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PGPASSWORD=zaq12wsx
+export PGPASSWORD=zaq12wsx
 
 psql -d gplates -U gplates -h localhost -c "create schema raster;"
 psql -d gplates -U gplates -h localhost < /tmp/crustal_thickness.sql
