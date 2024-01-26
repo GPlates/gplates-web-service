8631eb424c347685cba8a9793b6f1895dd3bdebf
michaelchin <michaelchin78@hotmail.com>
2024-01-26 20:31:37 +1100

    move some files into folder scripts

---
+++ scripts/create_tags.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: ./create-tag.py VERSION(such as 1.1.0)")
+    sys.exit(1)
+
+new_version = sys.argv[1]
+
+this_file_path = os.path.dirname(__file__)
+
+# 1. update version number in django/GWS/GWS/__init__.py
+# 2. commit change, create a tag and push changes and the new tag
+
+lines = []
+with open(f"{this_file_path}/../django/GWS/GWS/__init__.py", "r") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            lines.append(f'__version__ = "{new_version}"\n')
+        else:
+            lines.append(line)
+
+with open(f"{this_file_path}/../django/GWS/GWS/__init__.py", "w") as of:
+    of.writelines(lines)
+
+subprocess.call(["git", "add", f"{this_file_path}/../django/GWS/GWS/__init__.py"])
+
+files = [
+    f"{this_file_path}/../docker/gws-amd64/Dockerfile",
+    f"{this_file_path}/../docker/gws-arm64/Dockerfile",
+    f"{this_file_path}/../docker/gws-postgis-amd64/Dockerfile",
+    f"{this_file_path}/../docker/gws-postgis-arm64/Dockerfile",
+]
+
+for file in files:
+    lines = []
+    with open(file, "r") as f:
+        for line in f:
+            items = line.split(" ")
+            if (
+                len(items) > 1
+                and items[0].strip().lower() == "label"
+                and items[1].strip().lower().startswith("version")
+            ):
+                lines.append(f'LABEL version="{new_version}"\n')
+            else:
+                lines.append(line)
+
+    with open(file, "w") as of:
+        of.writelines(lines)
+    subprocess.call(["git", "add", file])
+
+
+subprocess.call(["git", "commit", "-m", f"update version to {new_version}"])
+
+subprocess.call(["git", "tag", "v" + new_version])
+subprocess.call(["git", "push", "origin", "--tags"])
--- test/setup-db.sh
+++ test/setup-db.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#
+# 👀👉 This script is meant to be used in test.yml.
+# 👀👉 If you would like to use it in other circumstances, make sure /tmp/crustal_thickness.sql exists.
+#
+
 export PGPASSWORD=gplates
 
 psql -d gplates -U gplates -h localhost -c "create schema raster;"
