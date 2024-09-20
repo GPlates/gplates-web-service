3d252ae7d3b08d3b52af4d3c2c37d6619e5a2f04
michaelchin <michaelchin78@hotmail.com>
2024-09-21 06:43:56 +1000

    minor changes

--- scripts/start-gws-dev.sh
+++ scripts/start-gws-dev.sh
@@ -3,4 +3,4 @@
 BASEDIR=$(dirname "$0")
 echo "$BASEDIR"
 cd $BASEDIR  
-docker compose -f ../docker/docker-compose-code-and-db-volume.yml run  --rm --service-ports gws /bin/bash -c "cd /gws/django/GWS && python3 manage.py runserver 0.0.0.0:80"
+docker compose -f ../docker/docker-compose-external-volumes.yml run  --rm --service-ports gws /bin/bash -c "cd /gws/django/GWS && python3 manage.py runserver 0.0.0.0:80"
