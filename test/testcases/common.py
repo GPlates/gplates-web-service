aaf77e06731b27a11f666a3792a2880d90024db7
michaelchin <michaelchin78@hotmail.com>
2024-12-09 21:22:05 +1100

    improve test_misc.py

--- test/testcases/common.py
+++ test/testcases/common.py
@@ -2,19 +2,20 @@ import logging
 import os
 import time
 from enum import Enum
+from functools import wraps
 from pathlib import Path
 
 import requests
 import urllib3
 
 
-def setup_logger(cls, name):
+def setup_logger(cls, name: str, level=logging.INFO):
     cls.logger = logging.getLogger(name)
-    cls.logger.setLevel(logging.INFO)
+    cls.logger.setLevel(level)
     cls.logger.propagate = False
     Path("logs").mkdir(parents=True, exist_ok=True)
     fh = logging.FileHandler(f"logs/{name}.log")
-    fh.setLevel(logging.INFO)
+    fh.setLevel(level)
     formatter = logging.Formatter(
         "%(asctime)s {%(pathname)s:%(lineno)d} \n\n%(message)s\n "
     )
@@ -25,15 +26,17 @@ def setup_logger(cls, name):
     urllib3.disable_warnings()
 
 
-def get_server_url(cls):
-    cls.SERVER_URL = os.getenv("GWS_SERVER_URL")
+def get_server_url(cls=None) -> str:
+    msg = "Using server URL in environment variable GWS_SERVER_URL: {}"
+    url = os.getenv("GWS_SERVER_URL")
+    if not url:
+        url = "http://127.0.0.1:18000"
+        msg = "Using default server URL {}"
 
-    if not cls.SERVER_URL:
-        cls.SERVER_URL = "http://127.0.0.1:18000"
-        # print(cls.logger)
-        cls.logger.info(f"Using server URL in script {cls.SERVER_URL}")
-    else:
-        cls.logger.info(f"Using server URL in environment variable {cls.SERVER_URL}")
+    if cls is not None:
+        cls.SERVER_URL = url
+        cls.logger.info(msg.format(cls.SERVER_URL))
+    return url
 
 
 def get_test_level():
@@ -61,16 +64,16 @@ class HttpMethod(Enum):
 
 
 def _send_request(
-    url: str, data: dict, delay: int = 0, method: HttpMethod = HttpMethod.GET
+    url: str, data: dict = {}, delay: float = 0.0, method: HttpMethod = HttpMethod.GET
 ) -> requests.Response:
     """a wrapper to send http(s) GET/POST request via 'requests'
     we can insert time delay for each request by using this wrapper"""
-    if delay > 0:
+    if delay > 0.0:
         time.sleep(delay)
     else:
         try:
-            d = int(os.getenv("GWS_TEST_DELAY", 0))
-            if d > 0:
+            d = float(os.getenv("GWS_TEST_DELAY", 0.0))
+            if d > 0.0:
                 time.sleep(d)
         except:
             pass
@@ -87,7 +90,7 @@ def _send_request(
     elif method == HttpMethod.POST:
         return requests.post(
             url,
-            params=data,
+            data=data,
             verify=False,
             proxies={
                 "http": ""
@@ -97,9 +100,29 @@ def _send_request(
         raise Exception(f"Unknown Http method: {method}")
 
 
-def send_get_request(url: str, data: dict, delay: int = 0) -> requests.Response:
-    return _send_request(url, data, delay, method=HttpMethod.GET)
+def send_get_request(
+    url: str, params: dict = {}, delay: float = 0.0
+) -> requests.Response:
+    return _send_request(url, params, delay, method=HttpMethod.GET)
 
 
-def send_post_request(url: str, data: dict, delay: int = 0) -> requests.Response:
+def send_post_request(
+    url: str, data: dict = {}, delay: float = 0.0
+) -> requests.Response:
     return _send_request(url, data, delay, method=HttpMethod.POST)
+
+
+def add_server_url_to_docstring():
+    """add the server's URL to the function's __doc__"""
+
+    def inner(func_pointer):
+        if func_pointer.__doc__:
+            func_pointer.__doc__ = func_pointer.__doc__.format(get_server_url())
+
+        @wraps(func_pointer)
+        def wrapper(*args, **kwargs):
+            return func_pointer(*args, **kwargs)
+
+        return wrapper
+
+    return inner
--- test/testcases/test_misc.py
+++ test/testcases/test_misc.py
@@ -3,8 +3,12 @@ import logging
 import unittest
 from pathlib import Path
 
-import requests
-from common import get_server_url, send_get_request, setup_logger
+from common import (
+    add_server_url_to_docstring,
+    get_server_url,
+    send_get_request,
+    setup_logger,
+)
 
 # python3 -m unittest -vv test_misc.py
 
@@ -18,142 +22,130 @@ class MiscTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        setup_logger(cls, Path(__file__).stem)
+        setup_logger(cls, Path(__file__).stem, logging.INFO)
         get_server_url(cls)
 
     @classmethod
     def tearDownClass(cls):
         cls.logger.info("tearDownClass")
 
+    @add_server_url_to_docstring()
     def test_motion_path(self):
-        """test motion path"""
-        # time.sleep(1)
+        """testing {}/reconstruct/motion_path?seedpoints=0,0&movplate=701"""
+
         data = {"seedpoints": "0,0", "movplate": 701}
-        r = requests.get(
+        r = send_get_request(
             self.SERVER_URL + "/reconstruct/motion_path",
             params=data,
-            verify=False,
-            proxies=self.proxies,
         )
+        self.logger.debug(r.request.url + str(r.request.headers))
         self.assertEqual(r.status_code, 200)
-        if r.status_code == 200:
-            logging.info(json.dumps(json.loads(str(r.text)), sort_keys=True, indent=4))
-            self.logger.info("PASSED! test motion path")
-        else:
-            raise Exception("FAILED: " + r.request.url + str(r.request.headers))
 
+        logging.info(
+            "######### test_motion_path ###########\n"
+            + json.dumps(json.loads(str(r.text)), sort_keys=True, indent=4)
+            + "######### test_motion_path ###########\n"
+        )
+
+    @add_server_url_to_docstring()
     def test_find_axis_and_angle(self):
-        """test find_axis_and_angle"""
-        # time.sleep(1)
+        """testing {}/earth/find_axis_and_angle?point_a=120,45&point_b=20,-45"""
         data = {"point_a": "120,45", "point_b": "20,-45"}
-        r = requests.get(
+        r = send_get_request(
             self.SERVER_URL + "/earth/find_axis_and_angle",
             params=data,
-            verify=False,
-            proxies=self.proxies,
         )
+        self.logger.debug(r.request.url + str(r.request.headers))
         self.assertEqual(r.status_code, 200)
-        if r.status_code == 200:
-            logging.info(json.dumps(json.loads(str(r.text)), sort_keys=True, indent=4))
-            self.logger.info("PASSED! test find_axis_and_angle")
-        else:
-            raise Exception("FAILED: " + r.request.url + str(r.request.headers))
 
+        logging.info(
+            "######### test_find_axis_and_angle ###########\n"
+            + json.dumps(json.loads(str(r.text)), sort_keys=True, indent=4)
+            + "######### test_find_axis_and_angle ###########\n"
+        )
+
+    @add_server_url_to_docstring()
     def test_interp_two_locations(self):
-        """test interp_two_locations"""
-        # time.sleep(1)
+        """testing {}/earth/interp_two_locations?point_a=120,45&point_b=20,-45&num=10"""
+
         data = {"point_a": "120,45", "point_b": "20,-45", "num": 10}
-        r = requests.get(
-            self.SERVER_URL + "/earth/interp_two_locations",
-            params=data,
-            verify=False,
-            proxies=self.proxies,
+        r = send_get_request(
+            self.SERVER_URL + "/earth/interp_two_locations", params=data
         )
+        self.logger.debug(r.request.url + str(r.request.headers))
         self.assertEqual(r.status_code, 200)
-        if r.status_code == 200:
-            logging.info(json.dumps(json.loads(str(r.text)), sort_keys=True, indent=4))
-            self.logger.info("PASSED! test interp_two_locations")
-        else:
-            raise Exception("FAILED: " + r.request.url + str(r.request.headers))
 
+        logging.info(
+            "######### test_interp_two_locations ###########\n"
+            + json.dumps(json.loads(str(r.text)), sort_keys=True, indent=4)
+            + "\n########## end of test_interp_two_locations ##########\n"
+        )
+
+    @add_server_url_to_docstring()
     def test_distance(self):
-        """test distance"""
-        # time.sleep(1)
+        """testing {}/earth/distance?point_a=120,45&point_b=20,-45"""
+
         data = {"point_a": "120,45", "point_b": "20,-45"}
-        r = requests.get(
-            self.SERVER_URL + "/earth/distance",
-            params=data,
-            verify=False,
-            proxies=self.proxies,
-        )
+        r = send_get_request(self.SERVER_URL + "/earth/distance", params=data)
+        self.logger.debug(r.request.url + str(r.request.headers))
         self.assertEqual(r.status_code, 200)
-        if r.status_code == 200:
-            logging.info(json.dumps(json.loads(str(r.text)), sort_keys=True, indent=4))
-            self.logger.info("PASSED! test distance")
-        else:
-            raise Exception("FAILED: " + r.request.url + str(r.request.headers))
 
-    def test_paleo_labels(self):
-        """test paleo-labels"""
-        # time.sleep(1)
-        data = {"time": 100, "model": "muller2019"}
-        r = requests.get(
-            self.SERVER_URL + "/earth/get_labels",
-            params=data,
-            verify=False,
-            proxies=self.proxies,
+        dist = json.loads(r.text)
+        self.assertAlmostEqual(dist["distance"], 14018, delta=1)
+        logging.info(
+            "######### test_distance ###########\n"
+            + json.dumps(dist, sort_keys=True, indent=4)
+            + "\n########## end of test_distance ##########\n"
         )
-        self.assertEqual(r.status_code, 200)
-        if r.status_code == 200:
-            self.logger.info(
-                json.dumps(json.loads(str(r.text)), sort_keys=True, indent=4)
-            )
-            self.logger.info("PASSED! test_paleo_labels")
-        else:
-            raise Exception("FAILED: " + r.request.url + str(r.request.headers))
 
-    def test_paleo_cities_1(self):
-        """test paleo-cities test_paleo_cities"""
-        # time.sleep(1)
-        data = {"time": 100, "model": "muller2019"}
-        r = requests.get(
+    @add_server_url_to_docstring()
+    def test_paleo_cities(self):
+        """testing {}/earth/get_cities?time=100&model=muller2019"""
+
+        r = send_get_request(
             self.SERVER_URL + "/earth/get_cities",
-            params=data,
-            verify=False,
-            proxies=self.proxies,
+            params={"time": 100, "model": "muller2019"},
         )
-        self.logger.info(r.request.url + str(r.request.headers))
+        self.logger.debug(r.request.url + str(r.request.headers))
         self.assertEqual(r.status_code, 200)
 
         return_data = json.loads(str(r.text))
-        self.logger.info(json.dumps(return_data, sort_keys=True, indent=4))
+        self.logger.info(
+            "######### test_paleo_cities ###########\n"
+            + json.dumps(return_data, sort_keys=True, indent=4)
+            + "\n########## end of test_paleo_cities ##########\n"
+        )
+
         self.assertEqual(len(return_data["names"]), len(return_data["lons"]))
         self.assertEqual(len(return_data["names"]), len(return_data["lats"]))
-        self.logger.info("PASSED! test_paleo_cities")
 
-    def test_paleo_cities_2(self):
-        """test paleo-cities get_present_day_cities"""
-        # time.sleep(1)
-        r = requests.get(
+    @add_server_url_to_docstring()
+    def test_get_present_day_cities(self):
+        """testing {}/earth/get_present_day_cities"""
+
+        r = send_get_request(
             self.SERVER_URL + "/earth/get_present_day_cities",
-            verify=False,
-            proxies=self.proxies,
         )
-        self.assertEqual(r.status_code, 200)
-        if r.status_code == 200:
-            self.logger.info(
-                json.dumps(json.loads(str(r.text)), sort_keys=True, indent=4)
-            )
-            self.logger.info("PASSED! test_paleo_cities_2")
-        else:
-            raise Exception("FAILED: " + r.request.url + str(r.request.headers))
+        self.logger.debug(r.request.url + str(r.request.headers))
 
+        self.assertEqual(r.status_code, 200)
+
+        self.logger.info(
+            "######### test_get_present_day_cities ###########\n"
+            + json.dumps(json.loads(str(r.text)), sort_keys=True, indent=4)
+            + "\n########## end of test_get_present_day_cities ##########\n"
+        )
+
+    @add_server_url_to_docstring()
     def test_paleo_labels(self):
-        """testing https://gws.gplates.org/earth/get_labels?time=300&model=merdith2021"""
+        """testing {}/earth/get_labels?time=300&model=merdith2021"""
+
         r = send_get_request(
             self.SERVER_URL + "/earth/get_labels",
-            data={"time": 300, "model": "merdith2021"},
+            params={"time": 300, "model": "merdith2021"},
         )
+        self.logger.debug(r.request.url + str(r.request.headers))
+
         self.assertEqual(r.status_code, 200)
 
         self.logger.info(
